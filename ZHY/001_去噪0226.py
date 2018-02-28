91711ed1e09c92c513be8ea0686cb4d57cb3544e
c of 8051 clock <a331588664@gmail.com>
2018-02-28 09:12:27 +0800

    Update 001_去噪0226.py
    Improve efficiency

--- ZHY/001_去噪0226.py
+++ ZHY/001_去噪0226.py
@@ -16,13 +16,17 @@ a = 1.1
 b = 30
 
 for i in range(rows):
-    for j in range(cols):
-        for c in range(3):
-            color = img[i, j][c] * a + b
-            if color > 255:
-                dst[i, j][c] = 255
-            elif color < 0:
-                dst[i, j][c] = 0
+    if（i%2）==0
+    continue
+        for j in range(cols):
+            if（i%2）==1
+            continue
+                for c in range(3):
+                    color = img[i, j][c] * a + b
+                    if color > 255:
+                        dst[i, j][c] = 255
+                    elif color < 0:
+                        dst[i, j][c] = 0
 
 cv2.imshow('dst', dst)
 cv2.waitKey(0)
